f3462b38914d09db8bfee773a3e839a12ff866e3
Jon Poulton <jpoulton@pm.me>
2024-10-14 22:16:55 +0100

    Use detektAll

--- build-logic/src/main/kotlin/nasa/gradle/ConventionDetekt.kt
+++ build-logic/src/main/kotlin/nasa/gradle/ConventionDetekt.kt
@@ -1,21 +1,18 @@
 package nasa.gradle
 
+import blueprint.recipes.DetektAll
 import blueprint.recipes.detektBlueprint
-import io.gitlab.arturbosch.detekt.Detekt
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.getValue
 import org.gradle.kotlin.dsl.provideDelegate
-import org.gradle.kotlin.dsl.withType
 
 class ConventionDetekt : Plugin<Project> {
   override fun apply(target: Project) = with(target) {
-    detektBlueprint()
-
-    tasks.withType<Detekt>().configureEach {
-      exclude { it.file.path.contains("generated") }
-    }
+    detektBlueprint(
+      detektAllConfig = DetektAll.Apply(ignoreRelease = true),
+    )
 
     val detektPlugins by configurations
     dependencies {
