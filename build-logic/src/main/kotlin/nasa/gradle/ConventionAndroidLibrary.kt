f1a26d9f3a469ecdefc7c95635c59e23d0958c3e
Jon Poulton <jpoulton@pm.me>
2024-10-25 18:00:30 +0100

    Disable instrumented tests if androidTest folder doesn't exist

--- build-logic/src/main/kotlin/nasa/gradle/ConventionAndroidLibrary.kt
+++ build-logic/src/main/kotlin/nasa/gradle/ConventionAndroidLibrary.kt
@@ -1,6 +1,7 @@
 package nasa.gradle
 
 import com.android.build.api.dsl.LibraryExtension
+import com.android.build.api.variant.LibraryAndroidComponentsExtension
 import com.android.build.gradle.LibraryPlugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
@@ -36,6 +37,13 @@ class ConventionAndroidLibrary : Plugin<Project> {
           enableUnitTestCoverage = false
           enableAndroidTestCoverage = false
         }
+      }
+    }
+
+    extensions.configure<LibraryAndroidComponentsExtension> {
+      // disable instrumented tests if androidTest folder doesn't exist
+      beforeVariants {
+        it.enableAndroidTest = it.enableAndroidTest && projectDir.resolve("src/androidTest").exists()
       }
     }
   }
