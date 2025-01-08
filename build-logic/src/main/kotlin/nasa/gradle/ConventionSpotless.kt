4289007de4244ce94fd1b68cab8172a8a34baf90
Jon Poulton <jpoulton@pm.me>
2025-01-08 18:45:02 +0000

    Fix spotless deprecation

--- build-logic/src/main/kotlin/nasa/gradle/ConventionSpotless.kt
+++ build-logic/src/main/kotlin/nasa/gradle/ConventionSpotless.kt
@@ -1,11 +1,36 @@
 package nasa.gradle
 
-import blueprint.recipes.spotlessBlueprint
+import com.diffplug.gradle.spotless.SpotlessExtension
+import com.diffplug.spotless.extra.wtp.EclipseWtpFormatterStep
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.getByType
 
 class ConventionSpotless : Plugin<Project> {
-  override fun apply(target: Project) = with(target) {
-    spotlessBlueprint()
+  override fun apply(target: Project): Unit = with(target) {
+    with(plugins) {
+      apply("com.diffplug.spotless")
+    }
+
+    extensions.getByType(SpotlessExtension::class).apply {
+      format("misc") {
+        target("*.gradle", "*.gitignore", "*.pro")
+        leadingTabsToSpaces()
+        trimTrailingWhitespace()
+        endWithNewline()
+      }
+      json {
+        target("*.json")
+        simple()
+      }
+      yaml {
+        target("*.yml", "*.yaml")
+        jackson()
+      }
+      format("xml") {
+        target("*.xml")
+        eclipseWtp(EclipseWtpFormatterStep.XML)
+      }
+    }
   }
 }
